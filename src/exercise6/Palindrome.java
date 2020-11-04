c8bd467d0079e3a27f65465feba87696deb53e3b
DenislavKirchev <denislav_delon@abv.bg>
2020-11-04 13:52:36 +0000

    .

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -2,11 +2,13 @@ package exercise6;
 
 import java.util.Scanner;
 
+
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
+        assert n >0;
         int[] array = new int[n];
         for (int i = 0; i < n; i++) {
           array[i] = input.nextInt();
