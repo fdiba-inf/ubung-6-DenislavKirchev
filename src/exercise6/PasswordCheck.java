05bd32cd0d9240f275dbd45537743f7e3187f3fa
DenislavKirchev <denislav_delon@abv.bg>
2020-11-09 10:05:24 +0000

    .

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -1,55 +1,27 @@
 package exercise6;
 
 import java.util.Scanner;
-import java.lang.Character;
-import java.util.*;
 
-public class PasswordCheck {
+
+public class Palindrome {
     public static void main(String[] args) {
-      
-        boolean validLength;
-        boolean check;
-        int count1=0;
-        int count2=0;
         Scanner input = new Scanner(System.in);
 
-        do{
-            System.out.println("Enter length: ");
-            
-            int length = input.nextInt();
-            if(length<8){
-                validLength=false;
-            }else{
-                validLength=true;
-            }
-            char[] password = new char [length];
-            System.out.println("Enter password: ");
-            for(int i = 0; i < password.length; i++){
-                password[i] = input.next().charAt(0);
-                if(Character.isDigit(password[i])){
-                    count1++;
-                }
-            }
-            for(int i = 0; i < password.length; i++){
-                if(!Character.isLetterOrDigit(password[i])){
-                    count2++;
-                }
+        int n = input.nextInt();
+        assert n > 0;
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+          array[i] = input.nextInt();
+        }
 
-            }
+        boolean palindrome = true;
 
-            if(count2>1 || count1<2){
-                check=false;
-            }else{
-                check=true;
-
-            }
-
-
-            if(validLength && check){
-                System.out.println("Password valid!");
-            }
-        }while(!validLength || !check);
-
-
+        for (int i = 0; i < (array.length + 1) / 2; i++) {
+          if (array[i] != array[n - i - 1]){
+            palindrome = false;
+          }
+        
+        }
+        System.out.println("Palindrome: " + palindrome);
     }
 }
